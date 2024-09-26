bb944d2439cc1ed0f2f06a91e47f076436f108b1
Helder <helderandrade15p@gmail.com>
2024-09-26 19:44:39 +0100

    (feat) add flappy duck

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 console.log("main.js");
+
+$(document).on("click", ".card", (event) => {
+    let $card = $(event.currentTarget)
+    let target = $(event.currentTarget).attr("target")
+    
+    $('.card').removeClass("selected");
+    $('.project').slideUp();
+
+    $card.addClass("selected");
+
+    $(target).parent().slideDown("fast", () => {
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        });
+    });
+});
