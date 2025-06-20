7a2e9e49ee6bc5ab9b99cc5536646d2355c8f08e
HelderZX <helderandrade15p@gmail.com>
2025-06-20 23:26:08 +0100

    style (general): redesign everything

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,40 @@ $(document).on("click", ".card", (event) => {
         });
     });
 });
+
+function slideHabilities() {
+    let habilities = $(".habilities span");
+    let currentIndex = 0;
+
+    function showHability(index) {
+        habilities.fadeOut(300, function() {
+            habilities.hide();
+            $(habilities[index]).fadeIn(300);
+        });
+    }
+
+    // Hide all except the first
+    habilities.hide();
+    $(habilities[0]).show();
+
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % habilities.length;
+        showHability(currentIndex);
+    }, 3000);
+}
+
+slideHabilities();
+
+$(window).on("scroll", () => {
+    if ($(window).scrollTop() > $(window).height() - 1) {
+        $(".nav").addClass("dark");
+    } else {
+        $(".nav").removeClass("dark");
+    }
+
+    if ($(window).scrollTop() > 50) {
+        $(".slide-down").fadeOut();
+    } else {
+        $(".slide-down").fadeIn();
+    }
+});
